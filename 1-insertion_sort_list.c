6f4d499fa5d11995182a5e2bf1e00b5c98dd4638
Eldad <eldadf456@gmail.com>
2024-02-18 14:51:26 -0800

    sorting_algorithms

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,51 @@
+#include "sort.h"
+
+/**
+ * swaping - swap nodes
+ * @a: first node
+ * @b: next node.
+ * Return: Empty
+ */
+void swaping(listint_t *a, listint_t *b)
+{
+	if (a->prev)
+		a->prev->next = b;
+	if (b->next)
+		b->next->prev = a;
+	a->next = b->next;
+	b->prev = a->prev;
+	a->prev = b;
+	b->next = a;
+}
+
+/**
+ * insertion_sort_list - insertion_sort_list.
+ * @list: insertion_sort_list.
+ * Return: Empty
+ */
+
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *i, *j;
+
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
+		return;
+	i = (*list)->next;
+	while (i)
+	{
+		j = i;
+		i = i->next;
+		while (j && j->prev)
+		{
+			if (j->prev->n > j->n)
+			{
+				swaping(j->prev, j);
+				if (j->prev == NULL)
+					*list = j;
+				print_list((const listint_t *)*list);
+			}
+			else
+				j = j->prev;
+		}
+	}
+}
---
+++ sort.h
@@ -0,0 +1,39 @@
+#ifndef SORT_H
+#define SORT_H
+
+#include <stddef.h>
+
+/**
+ * struct listint_s - Doubly list node
+ *
+ * @n: Intege node
+ * @prev: previous element list
+ * @next: next element list
+ */
+typedef struct listint_s
+{
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
+} listint_t;
+
+void print_array(const int *array, size_t size);
+void print_list(const listint_t *list);
+void bubble_sort(int *array, size_t size);
+void insertion_sort_list(listint_t **list);
+void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
+void shell_sort(int *array, size_t size);
+void counting_sort(int *array, size_t size);
+void merge_sort(int *array, size_t size);
+void heap_sort(int *array, size_t size);
+void radix_sort(int *array, size_t size);
+void bitonic_sort(int *array, size_t size);
+void quick_sort_hoare(int *array, size_t size);
+void cocktail_sort_list(listint_t **list);
+void bitcompare(int *array, int i, int j, int dir);
+void bitmerge(int *array, int low, int cnt, int dir);
+void bitsort(int *array, int low, int cnt, int dir, size_t size);
+void bitonic_sort(int *array, size_t size);
+
+#endif
